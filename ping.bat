bd08f839e6aa37b00f60779485d1660af4983ac4
5J3344 <143262765+5J3344@users.noreply.github.com>
2023-09-02 15:50:04 +0530

    Add files via upload

---
+++ ping.bat
@@ -0,0 +1,15 @@
+@echo off
+
+rem Ping the specified IP address or hostname
+ping 192.168.1.1
+
+rem Display the current network configuration
+ipconfig /all
+
+rem Display all active network connections
+netstat -an
+
+rem Trace the route to the specified IP address or hostname
+tracert 192.168.1.1
+
+pause
